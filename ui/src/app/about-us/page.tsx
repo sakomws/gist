638fd72dcca3c534f081cb220170d1a8488942ad
dhruvarora93 <darora@opengov.com>
2024-02-24 21:06:43 -0800

    more changes

---
+++ ui/src/app/about-us/page.tsx
@@ -0,0 +1,14 @@
+import styles from "../page.module.css";
+
+const AboutUs = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h1>Under Maintenance</h1>
+        <p>We apologize for the inconvenience. Please check back later.</p>
+      </div>
+    </main>
+  );
+};
+
+export default AboutUs;
--- ui/src/app/demo/page.tsx
+++ ui/src/app/demo/page.tsx
@@ -1,55 +1,70 @@
 "use client";
-import { ImageGallery } from "react-image-grid-gallery";
 import styles from "../page.module.css";
-
-const imagesArray = [
-  {
-    alt: "Image1's alt text",
-    caption: "Image1's description",
-    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-  },
-  {
-    alt: "Image2's alt text",
-    caption: "Image2's description",
-    src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-  },
-  {
-    alt: "Image3's alt text",
-    caption: "Image3's description",
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-  },
-  {
-    alt: "Image3's alt text",
-    caption: "Image3's description",
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-  },
-  {
-    alt: "Image3's alt text",
-    caption: "Image3's description",
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-  },
-  {
-    alt: "Image3's alt text",
-    caption: "Image3's description",
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-  },
-  {
-    alt: "Image3's alt text",
-    caption: "Image3's description",
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-  },
-  {
-    alt: "Image3's alt text",
-    caption: "Image3's description",
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-  },
-];
+import { useEffect, useState } from "react";
 
 export default function Demo() {
+  const [body, setBody] = useState("");
+  const [images, setImages] = useState<string[]>([]);
+  const [subject, setSubject] = useState<string[]>([]);
+  const [summary, setSummary] = useState([]);
+  const [sender, setSender] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/data")
+      .then((response) => response.json())
+      .then((data) => {
+        const img = [];
+        const subjects = [];
+        const bodies = [];
+        const senders = [];
+        const summaries = [];
+        data.forEach((dt) => {
+          img.push({
+            src: dt.image_url,
+            alt: "",
+            caption: "Image!!",
+          });
+          subjects.push(dt.subject);
+          bodies.push(dt.body);
+          senders.push(dt.sender);
+          summaries.push(dt.summary);
+        });
+        setImages(img);
+        setSubject(subjects);
+        setBody(bodies);
+        setSender(senders);
+        setSummary(summaries);
+      });
+  }, []);
+
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
-        <ImageGallery imagesInfoArray={imagesArray} columnWidth={230} />
+      <div className={styles.centerImages}>
+        {images.map((image, index) => (
+          <>
+            <div>Subject: {subject[index]}</div>
+            <div>
+              <p>Summary: {summary[index]}</p>
+            </div>
+            <div className={styles.flipCard}>
+              <div className={styles.flipCardInner}>
+                <div className={styles.flipCardFront}>
+                  <img
+                    key={index}
+                    src={image.src}
+                    alt={image.alt}
+                    height={200}
+                    width={200}
+                  />
+                </div>
+                <div className={styles.flipCardBack}>
+                  <p>{sender[index]}</p>
+                  <p style={{ marginTop: "10px" }}>{body[index]}</p>
+                </div>
+              </div>
+            </div>
+          </>
+        ))}
       </div>
     </main>
   );
